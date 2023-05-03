fdc18ddc99e7b19286f6f03535d271e5dbabae41
BrunoAraus <101360453+BrunoAraus@users.noreply.github.com>
2023-05-03 17:49:53 -0400

    carro corregido

--- core/views.py
+++ core/views.py
@@ -8,13 +8,7 @@ def principal(request):
     data = {'productos' : productos}
     return render(request, 'core/cliente/principal.html', data)
 
-<<<<<<< Updated upstream
 def detalle_producto(request, id):
     verProducto = Producto.objects.get(idProducto=id)
     data = {'verProducto' : verProducto}
     return render(request, 'core/cliente/detalle_producto.html', data)
-=======
-
-
-
->>>>>>> Stashed changes
