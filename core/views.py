b3d07140ee0856b7b25e789f31fcb89045e6d5dc
JoSeCalderon1234 <j.calderon@duocuc.cl>
2023-05-02 21:37:11 -0400

    LISTAR PRODUCTOS DEL MODELS.PY EN VISTA = LISTO
    Pude listar todos los productos del models.py a la vista principal del cliente. Mañana miércoles continuaré con el carrito junto a @BrunoAraus.

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
 def principal(request):
+    productos = Producto.objects.all()
+    data = {'productos' : productos}
+    return render(request, 'core/cliente/principal.html', data)
 
-    return render(request, 'core/cliente/principal.html')
 
