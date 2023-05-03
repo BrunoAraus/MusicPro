764b5f80fc42e1f3df542e2ca0f6dcda61ac62a2
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2023-05-03 15:35:20 -0400

    Implementación de vista producto
    falta corrección del css y añadir sentencias django

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import principal
+from .views import *
 
 urlpatterns = [
     path('', principal,name="principal"),
+    path('detalle_producto/<id>', detalle_producto, name="detalle_producto")
 ]
